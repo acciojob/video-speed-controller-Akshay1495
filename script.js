dd4b1074ffaa7c35cc41f842d6b48fde56234918
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-26 07:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
-const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const video = document.querySelector('.flex');
+const progressBar = document.querySelector('.progress__filled');
+const toggleButton = document.querySelector('.player__button');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackSpeedSlider = document.querySelector('input[name="playbackRate"]');
+const rewindButton = document.querySelector('.rewind');
+const skipButton = document.querySelector('.skip');
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = video.paused ? '►' : '❚ ❚';
+  toggleButton.textContent = icon;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function adjustVolume() {
+  video.volume = volumeSlider.value;
+}
+
+function adjustPlaybackSpeed() {
+  video.playbackRate = playbackSpeedSlider.value;
+}
+
+function rewind() {
+  video.currentTime -= 10;
+}
+
+function skipForward() {
+  video.currentTime += 25;
+}
+
+toggleButton.addEventListener('click', togglePlay);
+volumeSlider.addEventListener('input', adjustVolume);
+playbackSpeedSlider.addEventListener('input', adjustPlaybackSpeed);
+rewindButton.addEventListener('click', rewind);
+skipButton.addEventListener('click', skipForward);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
